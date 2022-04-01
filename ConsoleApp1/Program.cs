04ac6748f740b382b6ef925d88bbbf1a2a2808c1
jollyfellow7 <simonecompagnone7@gmail.com>
2022-04-01 18:00:32 +0200

    ottimizzazione codice

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string[] cibiPreferiti = {"Pizza","Pasta", "Pomodori", "Patate", "Hamburger", "Insalata", "Torta","Mela" };
+string[] cibiPreferiti = {"Pizza","Pasta", "Pomodori", "Patate", "Hamburger", "Insalata", "Torta","Snack" };
 var classifica = 1;
 //stampo a video la lunghezza del mio array
 Console.WriteLine("Questa è la classifica della top " + cibiPreferiti.Length + " dei miei cibi preferiti:");
@@ -29,16 +29,16 @@ for (int i = 0; i < cibiPreferiti.Length; i++) { //stampo la mia classifica
     
 }
 //dichiaro una variabile int per calcolare la posizione media del mio array
-int numero = 0;
+int numero = cibiPreferiti.Length / 2;
 //se è un numero pari allora stampiamo i due cibi che si tovano nel mezzo, poichè non c'è un unico valore medio
 if (cibiPreferiti.Length % 2 == 0)  
     {
-        numero = cibiPreferiti.Length / 2;
+       
         Console.WriteLine("Questi sono i cibi situati nel mezzo della mia classifica: " + cibiPreferiti[numero -1] + " e " + cibiPreferiti[numero]) ;
 
-    } else if (cibiPreferiti.Length % 2 == 1) //se è di numero dispari il nosto array, allora, stampiamo il nostro cibo situato nel mezzo
+    } else //se è di numero dispari il nosto array, allora, stampiamo il nostro cibo situato nel mezzo
     {
-        numero = cibiPreferiti.Length / 2;
+        
         Console.WriteLine("Questo è il cibo situato nel mezzo della classifica: " + cibiPreferiti[numero]);
     }
 
